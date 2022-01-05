ba706090a69ed2ee4e7b985da3f8e91ed93e98e1
zpf <zpf518518@gmail.com>
2022-01-05 14:05:40 +0800

    feat: add queue/hashtable/linkedlist

--- typescript-algorithms/src/data-structures/linked-list/LinkedListNode.ts
+++ typescript-algorithms/src/data-structures/linked-list/LinkedListNode.ts
@@ -1,5 +1,5 @@
 export default class LinkedListNode {
-	constructor(public value: any, public next: LinkedListNode | null = null) {}
+	constructor(public value?: any, public next: LinkedListNode | null = null) {}
 	public toString(callback?: Function) {
 		return callback ? callback(this.value) : `${this.value}`;
 	}
